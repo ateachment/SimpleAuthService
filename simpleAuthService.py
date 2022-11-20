0e8cfd79636f9c4896a2a1453e878e8ddcbd037b
Wolfhard Eick <w@eick-at.de>
2022-11-20 13:51:43 +0100

    first commit

---
+++ simpleAuthService.py
@@ -0,0 +1,36 @@
+from flask import Flask, request
+import json
+
+# make cross-origin AJAX possible because of using swagger editor
+# https://flask-cors.readthedocs.io/en/latest/#using-json-with-cors
+from flask_cors import CORS, cross_origin  
+ 
+app = Flask(__name__)
+CORS(app)  # make cross-origin AJAX possible
+
+@app.route('/auth/user/login', methods=['POST'])
+def loginUser():
+    content_type = request.headers.get('Content-Type')
+    if content_type == 'application/json':
+        print(request)
+        username = request.json['username']
+        password = request.json['password']
+    elif (content_type == 'application/x-www-form-urlencoded'):     # regular html form data
+        username = request.form['username']
+        password = request.form['password']
+    else:
+        return 'Content-Type not supported: ' + content_type, 400   # Bad request
+    
+    if username == "testUser" and password == "testPwd":            # Everything is OK
+        return "{ \"token\": \"123456\" }"                          # JavaScript JSON parse don’t support single quote.
+    else:
+        return "{ \"token\": \"-1\" }", 403            # Forbidden
+
+
+@app.route('/auth/user/<token>', methods=['DELETE'])
+def logoutUser(token):
+    return "{ \"token\": \"-1\" }" # 200 logout sucessful
+
+
+if __name__ == '__main__':
+    app.run()
---
+++ testSimpleAuthService.py
@@ -0,0 +1,39 @@
+import pytest
+import json
+from simpleAuthService import app
+
+def test_login_json_sucess():
+    data =  {
+                "username": "testUser",
+                "password": "testPwd"
+            }
+    response = app.test_client().post('/auth/user/login', json=data)
+    assert response.status_code == 200
+    token = json.loads(response.data.decode('utf-8')).get("token")
+    assert token == '123456'
+
+def test_login_json_fail():
+    data =  {
+                "username": "testUser",
+                "password": "testWrongPwd"
+            }
+    response = app.test_client().post('/auth/user/login', json=data)
+    assert response.status_code == 403
+    token = json.loads(response.data.decode('utf-8')).get("token")
+    assert token == '-1'
+
+def test_login_form_success():
+    data = "username=testUser&password=testPwd"
+    headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+    response = app.test_client().post('/auth/user/login', data=data, headers=headers)
+    assert response.status_code == 200
+    token = json.loads(response.data.decode('utf-8')).get("token")
+    assert token == '123456'
+
+def test_logout():
+    response = app.test_client().delete('/auth/user/123456')
+    assert response.status_code == 200
+    token = json.loads(response.data.decode('utf-8')).get("token")
+    assert token == '-1'
+
+
