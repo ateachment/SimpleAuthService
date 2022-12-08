991d7d7e520bd519cfa1cc3335a3e3af7bb5e5a7
Wolfhard Eick <w@eick-at.de>
2022-12-08 21:20:30 +0100

    added blocked list of revoked jwts

--- simpleAuthService.py
+++ simpleAuthService.py
@@ -21,11 +21,6 @@ if settings.DEBUG_MODE:
 #################################################################
 # user interface
 
-def decodeJWT(encoded_token):
-    public_key = serialization.load_pem_public_key(settings.PUBLIC_KEY, backend=default_backend())
-    decoded_token = jwt.decode(encoded_token, public_key, algorithms=["RS256"])
-    return decoded_token
-
 def roles(roleIDs):
     roles = []
     for roleID in roleIDs:
@@ -36,65 +31,56 @@ def roles(roleIDs):
         else:
             raise Exception("RoleID %i not defined")
     return roles
-            
+
+def decodeJWT(encoded_token):
+    public_key = serialization.load_pem_public_key(settings.PUBLIC_KEY, backend=default_backend())
+    decoded_token = jwt.decode(encoded_token, public_key, algorithms=["RS256"])
+    return decoded_token
+
+def standard_response(page, token):
+    decoded_token = decodeJWT(encoded_token=token)
+    roleIDs = decoded_token.get("roleIDs")
+    resp = make_response(render_template(page, roles = roles(roleIDs)))
+    resp.set_cookie('token', token, httponly=True)                          # cookie can not be read by JavaScript (safer)
+    return resp
+
+def standard_get_response(page):
+    token = request.cookies.get('token')
+    token = json.loads(validate_and_update_token(token)[0])['token']        # validate and update token
+    if token == "-1":                                                       # decoding failed 
+        return render_template('login.html', message = "Please login.")
+    elif token == "-2":                                                     # expired
+        return render_template('login.html', message = "Your session has expired. Please login again.")
+    else:                                                                   # validated and updated -> Dashboard
+        return standard_response(page, token)
+
+
 
 @app.route('/')
 def index():
     token = request.cookies.get('token')
-    if token == "-1":
+    token = json.loads(validate_and_update_token(token)[0])['token']        # validate and update token
+    if token == "-1":                                                       # decoding failed 
         return render_template('login.html', message = "Please login.")
-    elif token == None:
-        return render_template('login.html', message = "Please login.")
-    else:
-        try:
-            decoded_token = decodeJWT(encoded_token=token)
-            roleIDs = decoded_token.get("roleIDs")
-            return make_response(render_template('dashboard.html', roles = roles(roleIDs)))
-        except jwt.ExpiredSignatureError:
-            return render_template('login.html', message = "Your session has expired. Please login again.")
-        except:
-            return render_template('login.html', message = "Please login.")
+    elif token == "-2":                                                     # expired
+        return render_template('login.html', message = "Your session has expired. Please login again.")
+    else:                                                                   # validated and updated -> Dashboard
+        return standard_response("dashboard.html", token)
 
 @app.route('/dashboard', methods=['POST', 'GET'])   
 def dashboard():
-    if request.method == 'POST':                    # called by form data of login.html
-        dictResponse = json.loads(loginUser()[0])   # loginUser returns i.e. ('{"token": jwt}', 200)
-        token = dictResponse['token']
-        if token == '-1':                           # authentification failed -> login form 
+    if request.method == 'POST':                                            # called by form data of login.html
+        token = json.loads(loginUser()[0])['token']                         # loginUser returns i.e. ('{"token": jwt}', 200)
+        if token == '-1':                                                   # authentification failed -> login form 
             return render_template('login.html', message = "Wrong username/password.")                     
-        else:                                       # ok -> load dashboard
-            try:
-                decoded_token = decodeJWT(encoded_token=token)
-                roleIDs = decoded_token.get("roleIDs")
-                resp = make_response(render_template('dashboard.html', roles = roles(roleIDs)))
-                resp.set_cookie('token',token)
-                return resp
-            except jwt.ExpiredSignatureError:
-                return render_template('login.html', message = "Your session has expired. Please login again.")
-            except:
-                return render_template('login.html', message = "Please login.")
-    if request.method == 'GET':               # links or redirected by login.html (already logged on)
-        token = request.cookies.get('token')
-        try:
-            decoded_token = decodeJWT(encoded_token=token)
-            roleIDs = decoded_token.get("roleIDs")
-            return make_response(render_template('dashboard.html', roles = roles(roleIDs)))
-        except jwt.ExpiredSignatureError:
-            return render_template('login.html', message = "Your session has expired. Please login again.")
-        except:
-            return render_template('login.html', message = "Please login.")
-        
-@app.route('/pages/<page>', methods=['GET'])   
+        else:                                                               # ok -> load dashboard
+            return standard_response("dashboard.html", token)
+    if request.method == 'GET':                                             # links or redirected by login.html (already logged on)
+        return standard_get_response("dashboard.html")
+
+@app.route('/pages/<page>', methods=['GET'])                                # standard page
 def load(page):
-    token = request.cookies.get('token')
-    try:
-        decoded_token = decodeJWT(encoded_token=token)
-        roleIDs = decoded_token.get("roleIDs")
-        return make_response(render_template(page, roles = roles(roleIDs)))
-    except jwt.ExpiredSignatureError:
-        return render_template('login.html', message = "Your session has expired. Please login again.")
-    except:
-        return render_template('login.html', message = "Please login.")
+    return standard_get_response(page)
 
 @app.route('/logout', methods=['POST', 'GET'])   
 def logout():
@@ -103,7 +89,6 @@ def logout():
         decoded_token = decodeJWT(encoded_token=token)
     except:
         pass
-    l
     resp = make_response(render_template('login.html', message="You have logged out."))
     resp.set_cookie('token','-1')
     return resp         
@@ -116,7 +101,7 @@ def logout():
 
 private_key = serialization.load_pem_private_key(settings.PRIVATE_KEY, password=settings.PASSWORD, backend=default_backend())
 
-jwt_blacklist = {}
+jwt_blockedlist = {}
 
 @app.route('/auth/user/login', methods=['POST'])
 def loginUser():
@@ -141,16 +126,47 @@ def loginUser():
             roleIDs.append(row[0])         
         expiry = dt.datetime.now(tz=timezone.utc) + dt.timedelta(seconds=10) # Expiration 10 seconds in the future     
         token = jwt.encode({"exp": expiry, "roleIDs": roleIDs }, private_key, algorithm="RS256")
-        jwt_blacklist.update({ token:expiry })
         return json.dumps({ "token": token }), 200                  # 200 OK
     else:
         return json.dumps({ "token": "-1" }), 403                   # 403 forbidden
-                            
-@app.route('/auth/user/<token>', methods=['DELETE'])
+           
+@app.route('/auth/user/<token>', methods=['PUT'])
+def validate_and_update_token(token):
+    try:
+        blocked = jwt_blockedlist.pop(token, None)
+        if blocked == None:
+            decoded_token = decodeJWT(encoded_token=token)
+            roleIDs = decoded_token.get("roleIDs")
+
+            expiry = dt.datetime.now(tz=timezone.utc) + dt.timedelta(seconds=10) # Expiration 10 seconds in the future     
+            token = jwt.encode({"exp": expiry, "roleIDs": roleIDs }, private_key, algorithm="RS256")
+            return json.dumps({ "token": token }), 200              # 200 token update OK
+        else:
+            return "{ \"token\": \"-1\" }", 403                     # 403 forbidden
+    except jwt.ExpiredSignatureError:                            
+        return "{ \"token\": \"-2\" }", 403                         # 403 forbidden (expired)
+    except:
+        return "{ \"token\": \"-1\" }", 403                         # 403 forbidden (decoding failed for some reasons)
+
+@app.route('/auth/user/<token>', methods=['DELETE'])                # logout
 def logoutUser(token):
-    if jwt_blacklist.get(token):
-        jwt_blacklist.pop(token)
+    try:
+        decodeJWT(encoded_token=token)
+        expiry = dt.datetime.now(tz=timezone.utc) + dt.timedelta(seconds=10) # Expiration 10 seconds in the future     
+        jwt_blockedlist.update({ token:expiry })                    # logged out -> token in blockedlist
+    except:
+        pass
     return "{ \"token\": \"-1\" }", 200                             # 200 logout sucessful
+
+@app.route('/auth/cleanUp', methods=['DELETE'])                     # Webhook: clean up blocked_token_lists
+def cleanUp_blocked_token_list():
+    now = dt.datetime.now(tz=timezone.utc) 
+    cnt = 0
+    for jwt in jwt_blockedlist:
+        if jwt.value > now:
+            jwt_blockedlist.pop(jwt)
+            cnt += 1
+    return "{ \"cleanedUp\": cnt }", 200 
 
 if __name__ == '__main__':
     app.run()
--- testSimpleAuthService.py
+++ testSimpleAuthService.py
@@ -6,6 +6,8 @@ from cryptography.hazmat.backends import default_backend
 import json
 from simpleAuthService import app
 
+encoded_token = ""
+
 def decodeJWT(encoded_token):
     public_key = serialization.load_pem_public_key(settings.PUBLIC_KEY, backend=default_backend())
     decoded_token = jwt.decode(encoded_token, public_key, algorithms=["RS256"])
@@ -41,16 +43,24 @@ def test_login_form_success():
     response = app.test_client().post('/auth/user/login', data=data, headers=headers)
     assert response.status_code == 200
 
+    global encoded_token 
     encoded_token = json.loads(response.data.decode('utf-8')).get("token")
     decoded_token = decodeJWT(encoded_token=encoded_token)
 
     roleIDs = decoded_token.get("roleIDs")
     assert roleIDs == [1, 2]
 
-
 def test_logout():
-    response = app.test_client().delete('/auth/user/123456')
+    response = app.test_client().delete('/auth/user/' + encoded_token)
     assert response.status_code == 200
     token = json.loads(response.data.decode('utf-8')).get("token")
     assert token == '-1'
+
+
+def cleanUp_blocked_token_list():
+    assert "x" == encoded_token
+    response = app.test_client().delete('/auth/cleanUp')
+    assert response.status_code == 200
+    cleanedUp = json.loads(response.data.decode('utf-8')).get("cleanedUp")
+    assert cleanedUp == 1
 
