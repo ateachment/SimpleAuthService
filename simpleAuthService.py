5c37fd90a3e001a2a68b2c9eb8a7aced41858f48
Wolfhard Eick <w@eick-at.de>
2023-02-04 14:02:10 +0100

    Code line cleaned up

--- simpleAuthService.py
+++ simpleAuthService.py
@@ -115,7 +115,6 @@ def loginUser():
     
     db1 = db.Db()
     ph = PasswordHasher()
-    hashedPW = ph.hash(str(password))
     query = "SELECT userId, pwd FROM tblUser WHERE username='%s'" %(username)
     result = db1.execute(query)
     if(result):
